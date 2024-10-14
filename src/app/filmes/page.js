d6a2fdcfd3e260b263ae8f09154b9e0d85df9369
PedroMF <pedrolucas3049@gmail.com>
2024-10-13 21:15:43 -0300

    Começo da pagina de filmes

---
+++ src/app/filmes/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Pagina from "./components/Pagina";
+
+export default function Page() {
+  return (
+    <Pagina titulo="Avaliação de Filmes">
+        <div>
+            
+
+        </div>
+    </Pagina>
+  );
+}
